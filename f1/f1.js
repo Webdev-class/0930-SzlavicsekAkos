e497ec0be3d2f1f02c86b2fb641cbbdd0e13c143
SzlavicsekAkos <szlavicsek.akos@gmail.com>
2024-10-01 10:22:40 +0200

    Feladatok fent

--- f1/f1.js
+++ f1/f1.js
@@ -2,6 +2,12 @@ const students = require('../students.json');
 
 //Írj függvényt annak kiszűrésére, hogy a tanulók melyik emeleten laknak.
 function f1(floor) {
-   //TODO
+   return students.filter(
+      (students) =>{
+      return students.dormitoryFloor === floor
+   }
+)
+
 }
+console.log(f1(2))
 module.exports = f1;
